6e5a561583ccb40cb95351b80ecd21ac79a0a202
Stephen Brewer <stephenbrewer16@gmail.com>
2019-03-05 21:57:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-var number = 10
-
-function add5() {
-  number += 5
-}
-function divideBy3(){
-  number /= 3
-}
-divideBy3()
-
-console.log(number)
-
-add5()
-
-console.log(number)
+1 + 80
+60 - 40
+2 * 3.4
+5.0 / 2.5
